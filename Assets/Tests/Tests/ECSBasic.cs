1537c5e4f24b9944092d762523439df0bf71684c
Jay Bretherton <jake.bretherton05@gmail.com>
2022-11-22 03:41:45 +0000

    test added

---
+++ Assets/Tests/Tests/ECSBasic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ECSBasic
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void ECSBasicSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+   
+}
